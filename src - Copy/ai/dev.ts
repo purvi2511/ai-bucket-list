e1044ae6f4b6dabbd2742dba27dcb16164ce833b
Hegde <2438076@cognizant.com>
2025-08-13 09:02:28 +0530

    test-prod

--- src - Copy/ai/dev.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-bucket-list.ts';
-import '@/ai/flows/suggest-activity-timing.ts';
-import '@/ai/flows/estimate-activity-cost.ts';
-import '@/ai/flows/generate-activity-image.ts';
